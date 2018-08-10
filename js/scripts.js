3fdccef87b8cd8a147ae9f7c1f84dfbd41b7834c
Mahat <mahatabdi20@gmail.com>
2018-08-10 11:25:48 +0300

    add changes

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var length = parseInt(prompt("enter a length in centimeters"));
+var width = parseInt(prompt("enter a width in centimeters"));
+var height = parseInt(prompt("enter a height in centimeters"));
+if(length===width && length===height && width===height){
+  alert("This is an equilateral triangle!")
+};
+else if(length===width && width===height || length!==height){
+  alert("This is an isosceles triangle!")
+};
+else if(length!== width && width!== height && height!==length){
+  alert("This is a scalene triangle!")
+};
+else(isNaN){
+  alert("Not a triangle")
+};
