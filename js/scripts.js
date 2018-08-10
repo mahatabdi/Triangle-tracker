ecd17ff348070de4662d6cded5dfbdd81ca21ba4
Mahat <mahatabdi20@gmail.com>
2018-08-10 14:46:07 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
-function determine(){
+//function determine(){
 
-var length=parseFloat(prompt"Enter length");
-var width=parseFloat(prompt"Enter width");
-var height=parseFloat(prompt"Enter height");
+var length=parseFloat(prompt("Enter length"));
+var width=parseFloat(prompt("Enter width"));
+var height=parseFloat(prompt("Enter height"));
 
-if(length===width && length===height && width===height){
+if((length+width)<=height || length+height<=width || width+height<=length){
+  alert("It is not a triangle");
+}
+else if(length===width && length===height && width===height){
   alert("It is an equilateral triangle");
 }
 
 else if(length===width || length===height || width===height){
   alert("It is an isosceles triangle");
 }
-
-else if((length+width)<=height || length+height<=width || width+height<=length){
-  alert("It is not a triangle");
+else{
+  alert("scalene");
 }
-else(){
-  alert("Not a triangle")
-}
- }
-deteremine();
+// }
